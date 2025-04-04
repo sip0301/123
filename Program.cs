2cca3efa3d204c565d05de6ed2b9abe79b00a874
sip0301 <mariasabaeva451@gmail.com>
2025-04-04 12:21:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Задание_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //    float a = 3.2f;
+            //    decimal b = 2.6m;
+            //    double c = 4.5;
+            //    c = (double)a + (double)b + c;
+            //    Console.WriteLine(c);
+
+
+            //    int d = 3;
+            //    double e = 2.6;
+            //    double f = (double)d + e;
+            //    Console.WriteLine(f);
+            //    Console.Write("g = ");
+            //    int g = int.Parse(Console.ReadLine());
+
+
+            //Console.WriteLine(g*g);
+            //    double a = double.Parse(Console.ReadLine());
+            //    double b = Math.Round(a, 3);
+            //    Console.WriteLine(b);
+
+            //int a = int.Parse(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine("вы ввели число");
+            //Console.WriteLine(a);
+
+            //int b = int.Parse(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine(b);
+            //Console.WriteLine("вы ввели число");
+
+            //Console.WriteLine(" 1 13 49");
+            //Console.WriteLine("7 15 100");
+
+            //float z; int a =1, b = 2, c =3 ;
+            //const int m = 3 ;
+            //z = (a + b +c ) / m;
+            //Console.WriteLine( z );
+
+            //double a = int.Parse(Console.ReadLine());
+            //double b = int.Parse(Console.ReadLine());
+            //double c = (a + b ) / 2;
+            //Console.WriteLine(c);
+
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //double c = double.RootN(a, b);
+            //Console.WriteLine(c);
+
+            //int p = int.Parse(Console.ReadLine());
+            //int w = int.Parse(Console.ReadLine());
+            //int m =  p*w ;
+            //Console.WriteLine(m);
+            
+
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            if (a != 0)
+            { 
+                double x = -b / a;
+                Console.WriteLine(x);
+                Console.ReadLine();
+            }
+            else
+            { 
+                Console.WriteLine("ыфв");
+                Console.ReadLine();
+            }
+
+
+
+
+
+        }
+    }
+}
