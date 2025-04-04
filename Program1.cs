e3a50b34fbdc39a97b472becae7936e7892ffaf9
sip0301 <mariasabaeva451@gmail.com>
2025-04-04 12:29:52 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,20 @@
+﻿namespace задание3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Random rna = new Random();
+            Console.WriteLine(rna.Next());
+            Console.WriteLine(rna.Next(5));
+            Console.WriteLine(rna.Next(-5, 5));
+            Console.WriteLine(rna.Next(a + b));
+           
+
+        }
+    }
+}
