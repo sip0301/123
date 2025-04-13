058d8b030b142b41322d8437d1afca55bd7efde2
sip0301 <mariasabaeva451@gmail.com>
2025-04-14 00:41:12 +0400

    Create Практика 5 17.cs

---
+++ Практика 5 17.cs
@@ -0,0 +1,22 @@
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Praktika5
+//{
+//    internal class _15
+//    {
+//        static void Grades(int x1, int x2, int x3, int x4, int x5, int x6, int x7, int x8, int x9, int x10) 
+//        {
+//            int sum;
+//            sum = (x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10) / 10;
+//            Console.Write(sum);
+//        }
+//        public static void Main(string[] args)
+//        {
+//            Grades(4, 5, 2, 3, 4, 5, 2, 2, 3, 4);
+//        }
+//    }
+//}
