b64cdd8d59d0332a1cdcad5d76b1261b02fed6a4
sip0301 <mariasabaeva451@gmail.com>
2025-04-13 14:12:04 +0400

    Create Практика 4 2-3.cs

---
+++ Практика 4 2-3.cs
@@ -0,0 +1,20 @@
+﻿//namespace Praktika4
+//{
+//    internal class Program1
+//    {
+//        static void Main(string[] args)
+//        {
+//            double x;
+//            do
+//            {
+//                x = double.Parse(Console.ReadLine());
+//                if (x > 100 || x < 0)
+//                {
+
+//                    Console.WriteLine("не, вводи поновой");
+//                }
+                
+//            }while (x > 100 && x < 0);
+//        }
+//    }
+//}
