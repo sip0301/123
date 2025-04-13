2566fa90b2f950b2ba93834e1cc7a1bd1b26d8bf
sip0301 <mariasabaeva451@gmail.com>
2025-04-14 00:38:49 +0400

    Create Практика 5 12.cs

---
+++ Практика 5 12.cs
@@ -0,0 +1,25 @@
+﻿//namespace Parktika_5
+//{
+//    internal class Program
+//    {
+//        static void allnumerals(int n)
+//        {
+//            while (n > 0)
+//            {
+//                int z = n / 1000;
+//                int z1 = (n / 100) % 10;
+//                int z2 = (n / 10) % 10;
+//                int z3 = n % 10;
+//                Console.Write(z);
+//                Console.Write(z1);
+//                Console.Write(z2);
+//                Console.Write(z3);
+//                break;
+//            }
+//        }
+//        public static void Main(string[] args)
+//        {
+//            allnumerals(600);
+//        }
+//    }
+//}
