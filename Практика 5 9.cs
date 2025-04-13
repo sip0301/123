478a2a1df24e34a047d7508de07d5eef807db0f0
sip0301 <mariasabaeva451@gmail.com>
2025-04-13 16:23:29 +0400

    Create Практика 5 9.cs

---
+++ Практика 5 9.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Praktika5
+//{
+//    internal class _9
+//    {
+//        static double sqrt(double n)
+//        {
+//            double sum = 0;
+//            double count = 1;
+//            while (count <= n)
+//            {
+//                sum = sum + (count * count);
+//                count = count + 1;
+//            }
+//            return sum;
+//        }
+
+//        public static void Main(string[] args)
+//        {
+//            double x = sqrt(7);
+//            Console.WriteLine(x); 
+//        }
+//    }
+//}
+////1 * 1 = 1, 2 * 2 = 1 + 3, 3 * 3 = 1 + 3 + 5, 4 * 4 = 1 + 3 + 5 + 7, …, n * n = 1 + 3 + 5 + 7 + 9 + … +( — 1).
