a3c74630d3ac09c628097be73cf539cfca495b25
sip0301 <mariasabaeva451@gmail.com>
2025-04-22 00:30:08 +0300

    Add files via upload

---
+++ Практика 5/Практика 5/18.cs
@@ -0,0 +1,31 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Praktika5
+//{
+//    internal class _18
+//    {
+//        static void Mass(int x, int x1, int x3)
+//        {
+//            int mass = 0;
+//            int count = 1;
+//            while (count <= 3)
+//            {
+//                mass = (x + x1 + x3) / count;
+//                count++;
+                
+
+//            }
+//            Console.Write(mass);
+
+//        }
+//        public static void Main(string[] args)
+//        {
+//            Mass(1, 7, 30);
+
+//        }
+//    }
+//}
---
+++ Практика 5/Практика 5/4.cs
@@ -0,0 +1,25 @@
+﻿//namespace Praktika5
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            static void Stars(int n) //n – параметр метода
+//            {
+//                int z;
+//                int i = 20;
+//                Console.Write("Вот количества 20 - ");
+//                Console.WriteLine(n);
+//                Console.ReadKey();
+//                Console.Clear();
+//                for (z = 1; z <= n; z++)
+//                {
+//                    Console.Write(i + " ");
+//                }
+//            }
+//            Stars(4);
+
+//        }
+
+//    }
+//}
