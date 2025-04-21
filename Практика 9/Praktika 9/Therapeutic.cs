6d259dc3c1995f37155312db562dcb82951ca080
sip0301 <mariasabaeva451@gmail.com>
2025-04-22 00:18:46 +0300

    Add files via upload

---
+++ Практика 9/Praktika 9/Therapeutic.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktika_9
+{
+    internal class Therapeutic
+    {
+        public readonly int departmentid;
+        public string name;
+        public Cardiology headOfDepartment;
+        public List<Doctor> doctors;
+        public bool[] beds;
+    }
+}
